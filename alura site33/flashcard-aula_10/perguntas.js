746753ae29d07e6abbe6ba71dbb8769f3c6a1174
gabiborowi <161840779+gabiborowi@users.noreply.github.com>
2024-11-01 10:35:55 -0300

    Add files via upload

---
+++ alura site33/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quanto tempo durou a Segunda Guerra?',
+    'Durou 4 anos (1914-1918)'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Programação',
+    'O que é o Phyton?',
+    'É uma linguagem de programação'
+)
